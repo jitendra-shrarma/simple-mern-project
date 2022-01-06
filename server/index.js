b4b93261d8931937b919a171f7b2d1e4d082dac8
Jitendra Shamra <jks9536097795@gmail.com>
2022-01-07 00:19:00 +0530

    databasse connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const app = express();
+const dbConfig = require("./connection");
+const mongoose = require("mongoose");
+
+const db = {};
+db.mongoose = mongoose;
+db.url = dbConfig.db_url;
+
+
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected");
+    })
+    .catch(err => {
+        console.log("Cannot connect", err);
+        process.exit();
+    }
+);
+
 
 app.get("/", (req, res) => {
     res.json({ message: "working api"});
